af297162ea8a7d38b7ae38f8139f2911ff205359
lihongpan1989 <562608410@qq.com>
2016-03-03 14:50:22 +0800

    删除ViewVC和storybord

--- Everives_S/Controller/User/Controller/LoginAndRegist/YRYJForgetPswViewController.m
+++ Everives_S/Controller/User/Controller/LoginAndRegist/YRYJForgetPswViewController.m
@@ -18,6 +18,7 @@
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor whiteColor];
     self.title = @"忘记密码?";
+    
 }
 
 - (void)didReceiveMemoryWarning {
--- Everives_S/Controller/User/Controller/LoginAndRegist/YRYJRegistViewController.m
+++ Everives_S/Controller/User/Controller/LoginAndRegist/YRYJRegistViewController.m
@@ -7,9 +7,17 @@
 //
 
 #import "YRYJRegistViewController.h"
+#import "CWSPublicButton.h"
+#import "JKCountDownButton.h"
 
+#define kDistance 10
+#define kTextFieldHeight 44
 @interface YRYJRegistViewController ()
 
+@property (nonatomic, strong) UITextField *tellText;
+@property (nonatomic, strong) UITextField *codeText;
+@property (nonatomic, strong) JKCountDownButton *getCodeBtn;
+@property (nonatomic, strong) CWSPublicButton *registBtn;//注册按钮
 @end
 
 @implementation YRYJRegistViewController
@@ -18,8 +26,28 @@
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor whiteColor];
     self.title = @"注册";
+
+    
+}
+- (void)buildUI
+{
+    //手机号码输入框
+    self.tellText = [self setTextFieldWithFrame:CGRectMake(kDistance, kDistance*2+64, kSizeOfScreen.width-2*kDistance, kTextFieldHeight) withPlaceholder:@"请输入您的手机号"];
+    [self.view addSubview:self.tellText];
+    
+    
 }
 
+#pragma mark - 快速创建输入框
+-(UITextField *)setTextFieldWithFrame:(CGRect)frame withPlaceholder:(NSString *)placehold
+{
+    UITextField *textField = [[UITextField alloc]initWithFrame:frame];
+    textField.placeholder = placehold;
+    textField.backgroundColor = [UIColor whiteColor];
+    textField.borderStyle = UITextBorderStyleRoundedRect;
+    textField.leftView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, 10, kTextFieldHeight)];
+    return textField;
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     
